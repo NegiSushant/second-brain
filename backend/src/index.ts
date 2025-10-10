d420efbedaffb5e45dd7a0cc2484f8bfebd017d5
NegiSushant <sushantsinghnegi1998@gmail.com>
2025-10-10 10:39:49 +0530

    cors origin fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,12 @@ import { router } from "./routes/routes";
 
 
 const app = express();
-app.use(cors())
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }))
+  
 app.use(express.json());
 app.use(cookieParser());
 
