88197b00f9589cca89fef5a4070b1d70da149eb1
Sushant Singh Negi <sushantsinghnegi1998@gmail.com>
2025-10-11 22:16:37 +0530

    brain dashbord

--- frontend/src/components/cards-demo-2.tsx
+++ frontend/src/components/cards-demo-2.tsx
@@ -1,39 +1,49 @@
 "use client";
 import { cn } from "../lib/utils";
 
+interface CardDemoProps {
+  title: string;
+  link: string;
+  type: string;
+  tags: string[];
+}
+export default function CardDemo({ title, link, type, tags }: CardDemoProps) {
+  console.log("title" + title);
+  // switch (type){
 
-export default function CardDemo() {
+  // }
+  const deleteContent = async() => {
+    
+
+  }
   return (
     <div className="max-w-xs w-full group/card">
       <div
         className={cn(
           " cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl  max-w-sm mx-auto backgroundImage flex flex-col justify-between p-4",
-          "bg-[url(https://images.unsplash.com/photo-1544077960-604201fe74bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1651&q=80)] bg-cover"
+          "bg-[url(/public/brainImage.png)] bg-cover"
         )}
       >
         <div className="absolute w-full h-full top-0 left-0 transition duration-300 group-hover/card:bg-black opacity-60"></div>
-        <div className="flex flex-row items-center space-x-4 z-10">
-          <img
-            height="100"
-            width="100"
-            alt="Avatar"
-            src="/manu.png"
-            className="h-10 w-10 rounded-full border-2 object-cover"
-          />
-          <div className="flex flex-col">
-            <p className="font-normal text-base text-gray-50 relative z-10">
-              Manu Arora
-            </p>
-            <p className="text-sm text-gray-400">2 min read</p>
-          </div>
+        <div className="w-full flex justify-between items-center">
+          <h3 className="font-medium text-base text-gray-50 z-10">{title}</h3>
+          <button className="font-medium text-gray-50" onClick={deleteContent}>Delete</button>
         </div>
         <div className="text content">
           <h1 className="font-bold text-xl md:text-2xl text-gray-50 relative z-10">
-            Author Card
+            {link}
           </h1>
           <p className="font-normal text-sm text-gray-50 relative z-10 my-4">
-            Card with Author avatar, complete name and time to read - most
-            suitable for blogs.
+            {tags.map((tag, index) => {
+              return (
+                <span
+                  key={index}
+                  className="mr-2 border-2 border-gray-100 px-2 rounded-full"
+                >
+                  #{tag.title}
+                </span>
+              );
+            })}
           </p>
         </div>
       </div>
--- frontend/src/pages/brain/Dashboard.tsx
+++ frontend/src/pages/brain/Dashboard.tsx
@@ -4,26 +4,67 @@ import axios from "axios";
 
 const API = import.meta.env.VITE_API_URL;
 
+interface contentSchema {
+  _id: string;
+  title: string;
+  link: string;
+  type: string;
+  tags: string[];
+}
+
 export const Dashboard = () => {
-  const [userBrain, setUserBrain] = useState([]);
-  useEffect(() => {
-    const userData = async () => {
-      const result = await axios.get(`${API}/content/content`, {
+  const [userBrain, setUserBrain] = useState<contentSchema[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getUserData = async () => {
+    try {
+      const response = await axios.get<{
+        message: string;
+        data: contentSchema[];
+      }>(`${API}/content/content`, {
         withCredentials: true,
       });
-      console.log("userbrain "+userBrain);
-      console.log("result "+result.data);
+      setUserBrain(response.data.data || []);
+      console.log(response);
 
-      setUserBrain(result.data.content);
-      console.log("userbrain: "+userBrain);
+      console.log(userBrain);
+    } catch (err) {
+      console.log(`Error while fetching data from server: ${err}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    getUserData();
+    const interval = setInterval(() => {
+      getUserData();
+    }, 10 * 1000);
+    return () => {
+      clearInterval(interval);
     };
-    userData();
-  }, [userBrain]);
+  }, []);
+
   return (
     <div className="flex flex-1 gap-2">
-      <CardDemo />
-      <CardDemo />
-      <CardDemo />
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <div className="flex flex-1 gap-2">
+          {userBrain.length > 0 ? (
+            userBrain.map((item) => (
+              <CardDemo
+                key={item._id}
+                title={item.title}
+                link={item.link}
+                type={item.type}
+                tags={item.tags}
+              />
+            ))
+          ) : (
+            <p>No content found.</p>
+          )}
+        </div>
+      )}
     </div>
   );
 };
